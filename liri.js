2933b62b98f8682ee26d555e112d15da325ed9d8
Derek Haworth <derek_haworth@bradycorp.com>
2018-01-31 15:44:12 -0600

    set up liri object with commands

--- liri.js
+++ liri.js
@@ -1,1 +1,26 @@
 require("dotenv").config();
+
+//set up liri object
+var liri {
+	fs: require("fs");
+
+	logic: function(userRequest) {
+		switch(userRequest) {
+    		case "my-tweets":
+    		 	console.log("chirp chirp");
+    		break;
+    		case "spotify-this-song":
+				console.log("spotifying");
+    		    break;
+    		case "movie-this":
+    			console.log("movie that");
+    		    break;
+    		case "do-what-it-says":
+    			console.log("did it");
+    		    break;
+    		default:
+		}
+	}
+
+
+}
