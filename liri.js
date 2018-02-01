2183e703ef915fd3676a9243259c68ee4b338622
Derek Haworth <derek_haworth@bradycorp.com>
2018-02-01 15:00:13 -0600

    write function for spotify songs

--- liri.js
+++ liri.js
@@ -1,18 +1,25 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+
 
 //set up liri object
 var liri = {
+
+    // Empty string for songs and movies
+    title: "",
+
 	fs: require("fs"),
 
 	logic: function(userRequest) {
 		switch(userRequest) {
     		case "my-tweets":
-    		 	console.log("chirp chirp");
+    		    console.log("chirp chirp");
                 this.twitter();
-    		break;
+    		    break;
 
     		case "spotify-this-song":
 				console.log("spotifying");
+                this.spotifySong(this.title);
     		    break;
 
     		case "movie-this":
@@ -20,31 +27,32 @@ var liri = {
     		    break;
 
     		case "do-what-it-says":
-    			console.log("did it");
+    			console.log("did it");                
     		    break;
 
     		default:
                 console.log("Try one of these arguments instead: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'");
 		}
+
 	},
 
     twitter: function() {
-        var keysFile = require("./keys.js");
         var twitter = require("twitter");
 
-        var client = new twitter(keysFile.twitter);
+        var twitterClient = new twitter(keys.twitter);
         var params = {
             screen_name: 'derekhaw1',
             count: 20
         };
 
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
 
             // console.log(tweets);
             // console.log(response);
 
             if (error) {
-                console.log("An error: " + error);
+                console.log("Error: " + error);
+                return;
             }
 
             var tweetLength = tweets.length;
@@ -52,21 +60,47 @@ var liri = {
             for (var i = 0; i < tweetLength; i++) {
                 var tweetNum = i + 1;
 
-                var builtTweet = {
+                var tweetObj = {
                     "number": tweetNum,
                     "created": tweets[i].created_at,
                     tweet: tweets[i].text
                 };
             }
 
-            console.log(builtTweet);
+            console.log(tweetObj);
 
         });
 
     }, 
 
-    spotifySong: function() {
+    spotifySong: function(songTitle) {
 
+        //if no song provided, default to Ace Of Base - The Sign
+        if (songTitle === "") {
+            songTitle = "Ace of Base - The Sign";
+        }
+
+        var spotify = require("node-spotify-api");
+        var spotifyClient = new spotify(keys.spotify);
+
+        spotifyClient.search({type: 'track', query: songTitle }, function(err,data) {
+
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            } 
+
+            // console.log(JSON.stringify(data, null, 2));
+
+            var trackObj = {
+                Artist: data.tracks.items[0].artists[0].name,
+                "Song Name": data.tracks.items[0].name,
+                "Preview Link": data.tracks.items[0].preview_url,
+                "Album": data.tracks.items[0].album.name
+            };
+    
+            console.log(JSON.stringify(trackObj, null, 2));
+        });
     },
 
     movieThis: function() {
